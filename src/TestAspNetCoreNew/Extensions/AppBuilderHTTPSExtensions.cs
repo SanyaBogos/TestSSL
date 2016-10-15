cac898d375e71f88a234530c30611ee4b66b00b7
SanyaBogos <bog_alex_ur@ukr.net>
2016-10-15 19:22:15 +0300

    Init commit

---
+++ src/TestAspNetCoreNew/Extensions/AppBuilderHTTPSExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using TestAspNetCoreNew.Service;
+
+namespace TestAspNetCoreNew.Extensions
+{
+    public static class AppBuilderHTTPSExtensions
+    {
+        public static IApplicationBuilder UseHttpsRedirectService(this IApplicationBuilder builder, X509Certificate2 x509certificate2)
+        {
+            return builder.UseMiddleware<HttpsRedirectService>(x509certificate2);
+        }
+    }
+}
---
+++ src/TestAspNetCoreNew/Service/HttpsRedirectService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAspNetCoreNew.Service
+{
+    public class HttpsRedirectService
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpsRedirectService(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.IsHttps)
+            {
+                await _next.Invoke(context);
+            }
+            else
+            {
+                context.Response.Redirect(string.Format("https://{0}{1}", context.Request.Host.ToString(), context.Request.Path.ToString()), true);
+            }
+        }
+    }
+}
